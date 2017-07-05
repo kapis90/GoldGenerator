ce4d12c48f7c17b32788c3f118cb4c119ccb0d60
Kapis <kapis90@gmail.com>
2017-07-05 21:12:40 +0200

    GUI first draft - working

--- GoldGenerator/GoldGenerator/GoldGenerator.py
+++ GoldGenerator/GoldGenerator/GoldGenerator.py
@@ -1,21 +1,54 @@
 import os
 import re
+import sys
 from shutil import copyfile
+from PyQt5 import QtCore, QtGui, QtWidgets
+from GoldGeneratorForm import Ui_GoldGeneratorForm
 
-#Looking for test dirs. Can be filtered by regexp
-test_names = [test for test in os.listdir(os.getcwd()) if re.search("BGA_color_map_exception.*",test)]
-#Pattern file used as source for gold files
-source = r'C:\Users\kkoltons\Documents\running_xPD_test\automation\clean_with_padstack\PKG\LogFiles\AIFImport.txt'
+class GoldGenerator(Ui_GoldGeneratorForm):
+    
+    def __init__(self, dialog):
+        Ui_GoldGeneratorForm.__init__(self)
+        self.setupUi(dialog)
 
-#Path to refs directory
-dst_dir = r'C:/Users/kkoltons/Documents/running_xPD_test/automation/refs/'
+        self.sourceBrowseButton.clicked.connect(self.browse_file)
+        self.refsBrowseButton.clicked.connect(self.browse_dir)
+        self.addButton.clicked.connect(self.browse_dirs)
+        self.removeButton.clicked.connect(self.remove)
+        self.generateButton.clicked.connect(self.generate)
 
-GF_ABREV = "messages"
+    def console_message(self, message):
+        self.consoleEdit.append(message)
 
-def copy_golds(source, dst_dir, GF_ABREV, test_names):
+    def browse_file(self):
+        self.source = QtWidgets.QFileDialog.getOpenFileName(dialog, 'Open File', '', '')
+        self.sourceEdit.setText(self.source[0])
+  
+    def browse_dir(self):
+        self.refs = QtWidgets.QFileDialog.getExistingDirectory(dialog, 'Open directory', '', QtWidgets.QFileDialog.ShowDirsOnly)
+        self.refsEdit.setText(self.refs)
 
-    for test_name in test_names:
-        copyfile(source, dst_dir + GF_ABREV + "." + test_name + ".gold")
+    def browse_dirs(self):
+        dir = QtWidgets.QFileDialog.getExistingDirectory(dialog, 'Point to local tests directory', '', QtWidgets.QFileDialog.ShowDirsOnly)
+        self.test_names = [test for test in os.listdir(dir) if os.path.isdir(os.path.join(dir, test))]
+        self.testListWidget.addItems(self.test_names)
+
+    def remove(self):
+        listItem = self.testListWidget.selectedItems()
+        for item in listItem:
+            self.testListWidget.takeItem(self.testListWidget.row(item))
+
+    def generate(self):
+        
+        for item in [str(self.testListWidget.item(i).text()) for i in range(self.testListWidget.count())]:
+            
+            copyfile(self.sourceEdit.text(), self.refsEdit.text() + "/" + self.gf_abrevEdit.text() + "." + item + ".gold")
+            self.console_message(item + " copied.")
 
 
-copy_golds(source, dst_dir, GF_ABREV, test_names)
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    dialog = QtWidgets.QDialog()
+    prog = GoldGenerator(dialog)
+    dialog.show()
+    sys.exit(app.exec_())
